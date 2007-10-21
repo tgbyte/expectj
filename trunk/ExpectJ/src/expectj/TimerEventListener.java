e5d32f9acf334fa77231dab28991c821c57d16dc
johanwalles <>
2007-10-21 21:53:39 +0000

    Cosmetics.

--- trunk/ExpectJ/src/expectj/TimerEventListener.java
+++ trunk/ExpectJ/src/expectj/TimerEventListener.java
@@ -5,17 +5,15 @@ package expectj;
  */
 
 public interface TimerEventListener {
-
     /**
      * This method represents the time-out event triggered by Timer.
      */
-    public void timerTimedOut();
+    void timerTimedOut();
 
     /**
      * This method is invoked by the Timer, when the timer thread
      * receives an interrupted exception.
      * @param reason Why we were interrupted.
      */
-    public void timerInterrupted(InterruptedException reason);
-
+    void timerInterrupted(InterruptedException reason);
 }
