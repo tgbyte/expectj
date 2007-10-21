cee715ff6bb36d50f1aa6a1ee4e98af4526fa716
johanwalles <>
2007-10-21 21:18:49 +0000

    Make the logging object private.

--- trunk/ExpectJ/src/expectj/SpawnedProcess.java
+++ trunk/ExpectJ/src/expectj/SpawnedProcess.java
@@ -29,7 +29,7 @@ public class SpawnedProcess implements TimerEventListener {
     private BufferedWriter out = null;
 
     // Debugger object
-    Debugger debug = new Debugger(SpawnedProcess.class, true);
+    private Debugger debug = new Debugger(SpawnedProcess.class, true);
 
     /**
      * This is what we're actually talking to.
